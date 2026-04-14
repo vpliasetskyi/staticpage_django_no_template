cdf39d26e962008c5e1bb250b8b809b03bb48225
vpliasetskyi <vpliasetskyi@gmail.com>
2026-04-13 18:44:03 -0700

    modify settings, views , and urls

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from static_pages_1 import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('contact/', views.contact, name="contact")
 ]
--- static_pages_1/views.py
+++ static_pages_1/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+nav = """
+    <nav>
+        <a href='/'>Home</a> |
+        <a href='contact/'>Contact</a>
+    </nav>
+"""
+name = "Tim"
+age = 24
+gains = 234.5634224
+
+home_body = f"""
+    <ol>
+        <li>Name: {name}</li>
+        <li>Age: {age}</li>
+        <li>Gains: {gains:.2f}</li>
+    </ol>
+    
+"""
+
+
+
+def home(request):
+    content= """
+            <h1>Welcome to my Page</h1>
+            <h2>Visit around</h2>
+            <p>Enjoy the content</p>
+    """
+    
+    return HttpResponse(nav + content + home_body)
+
+def contact(request):
+    return HttpResponse(nav + "Contact Us")
+
 
 # Create your views here.
